beca831a3a74fa16c81efd8897fac37df0bd0d25
jianghao <jianghao19941020@aliyun.com>
2019-04-30 17:22:40 +0800

    提交基础部分

---
+++ day-2/src/main/java/com/github/basis/CharacterDemo.java
@@ -0,0 +1,8 @@
+package com.github.basis;
+
+public class CharacterDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!ÄãºÃ£¬ÖÐ¹ú£¡");
+    }
+}
---
+++ day-5/src/main/java/com/github/basis/ReviewTest.java
@@ -0,0 +1,25 @@
+package com.github.basis;
+
+public class ReviewTest {
+
+    public static void main(String[] args) {
+        int sum1;
+        int sum2;
+        //for(int i = 1;i <= 100;i++){
+        if (4 % 2 != 0) {
+            //System.out.println(i);
+            sum1 = 1;
+        } else {
+            sum1 = 2;
+        }
+        //}
+        System.out.println(sum1);
+
+        if (false) {
+            sum2 = 1;
+        } else {
+            sum2 = 2;
+        }
+        System.out.println(sum2);
+    }
+}
